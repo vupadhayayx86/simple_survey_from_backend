f1ddd9e317f7645bd5c929403eef33fd12c77e43
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-05-04 14:34:06 +0530

    Credentials added origin middleware

--- routes/users_route.js
+++ routes/users_route.js
@@ -25,7 +25,7 @@ router.post("/",async (req,res)=>{
 
     await user.save()
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.cookie('jwtCookie',"dummydataaasdasfahsfdf",{httpOnly:false,maxAge:60*60*3*1000,sameSite:'none'})
+    res.cookie('jwtCookie',"dummydataaasdasfahsfdf",{httpOnly:false,maxAge:60*60*3*1000,sameSite:'none',secure})
     res.send(user)
 })
 
