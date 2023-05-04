9253d52e349a8cc5c446e7d78ddcd86c60a11470
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-05-04 17:50:23 +0530

    Cookie Parser Installed 2

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mongoose=require('mongoose')
 const user_route=require("./routes/users_route")
 const cors=require('cors')
 const cookieParser=require('cookie-parser')
-
+app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://surveyfrontend1.onrender.com",
+    origin:"https://surveyfrontend1.onrender.com/users",
     credentials:true,
 }))
 
