e8385805793a499d836e83f29d09dcc2972a9c7f
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-05-04 14:27:50 +0530

    Credentials added origin middleware

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://surveyfrontend1.onrender.com/");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
