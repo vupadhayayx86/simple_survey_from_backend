9a4456309d4ee649b00d86092ef7dcce89d77230
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-05-04 18:41:28 +0530

    Cookie Parser Installed 5

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors=require('cors')
 const cookieParser=require('cookie-parser')
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
     credentials:true,
 }))
 app.use(cookieParser())
