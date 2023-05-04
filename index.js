6c8f20b7e48fef6b732a4d39edd0832c4d7b8e3c
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-05-04 14:52:11 +0530

    Headersset in post

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ app.use(cors({
     credentials:true,
 }))
 
-app.use((req, res, next) => {
-  //header("Access-Control-Allow-Origin: http://origin.domain:port"); header("Access-Control-Allow-Credentials: true"); header("Access-Control-Allow-Methods: GET, POST"); header("Access-Control-Allow-Headers: Content-Type, *");
-  
-  res.header(
-    "Access-Control-Allow-Credentials: true",
-    "Access-Control-Allow-Origin:*",
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Methods: GET, POST",
-    "Access-Control-Allow-Headers: Content-Type, *",
-  );
-  next();
-});
+
 app.use(express.json())
 app.use("/users",user_route)
 
